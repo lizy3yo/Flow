007bf6d541fbf92fc1779c68055dd31631945bb0
unknown <dejesuskharl32@gmail.com>
2025-06-09 00:55:01 +0800

    Updated database connection settings for production

--- api/db.php
+++ api/db.php
@@ -2,10 +2,10 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-define('DB_HOST', '');
-define('DB_USER', '');
-define('DB_PASS', '');
-define('DB_NAME', '');
+define('DB_HOST', 'bxokhwhcr2i5ypfcp4ma-mysql.services.clever-cloud.com');
+define('DB_USER', 'umet7utvr4tkxiyq');
+define('DB_PASS', 'WVoyd7PJ89GWIfQllrtK');
+define('DB_NAME', 'bxokhwhcr2i5ypfcp4ma');
 
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 if ($conn->connect_error) {
