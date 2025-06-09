6fef145f6908d76c8d641c0f2cf372fb763e138b
wintah <153730882+lizy3yo@users.noreply.github.com>
2025-06-09 18:39:02 +0800

    Update router.js
    qwe

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import LoginSelector from '@/pages/LoginSelector.vue';
 import AdminLogin from '@/pages/AdminLogin.vue';
 import UserLogin from '@/pages/UserLogin.vue';
@@ -135,7 +135,7 @@ const routes = [
 ];
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes
 });
 
